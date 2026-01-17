8345993e9ac8e54a4cd08abe0335befc8fa05720
Gian <128049220+kyso1@users.noreply.github.com>
2026-01-17 10:29:17 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @name TFT Client
+ * @author Kyso
+ * @description TFT Client for league of legends client using pengu loader
+ * @link https://github.com/kyso1/TFT-Client
+ */
+
+import './css/tft.css';
